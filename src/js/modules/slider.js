8f5e0997949e79b49e2a3e79a656653a31301e34
Vladimir Prokofev <vladimir.prokofev@softgamings.com>
2025-03-09 13:12:45 +0300

    Loan init - add slider

---
+++ src/js/modules/slider.js
@@ -0,0 +1,48 @@
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page);
+        this.slides = this.page.children;
+        this.btns = document.querySelectorAll(btns);
+        this.slideIndex = 1;
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+
+        Array.from(this.slides).forEach(slide => slide.style.display = 'none');
+
+        Array.from(this.slides)[this.slideIndex - 1].style.display = 'block';
+    }
+
+    changeSlide(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render() {
+        console.log(this.slides);
+        console.log(this.btns);
+
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.changeSlide(1);
+            })
+
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            })
+        })
+
+        this.showSlides(this.slideIndex);
+
+
+    }
+}
